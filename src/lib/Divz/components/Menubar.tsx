074092990026383e83543f2cd1030be8e9dd2cd1
jaffamonkey <paullittlebury@gmail.com>
2025-07-15 23:21:00 +0100

    updates

--- src/lib/Divz/components/Menubar.tsx
+++ src/lib/Divz/components/Menubar.tsx
@@ -7,28 +7,37 @@ export const Menubar: React.FC = () => {
     setVisible(prev => !prev);
   };
 
-  const [demo, setDemo] = useState<number>(1);
+  const [, setDemo] = useState<number>(1);
 
-  const isActive = (link: number) => {
-    return link === demo ? "active" : "";
-  };
+  // const isActive = (link: number) => {
+  //   return link === demo ? "active" : "";
+  // };
+
+  function ActionLink() {
+    const handleClick = (e: { preventDefault: () => void; }) => {
+      e.preventDefault();
+      setDemo(3)
+    }
+    return (
+      <a className="fa fa-camera" onClick={handleClick} />
+    );
+  }
 
   return (
+
     <>
+
       <button className="toggle-button" onClick={toggleMenubar}>
         ☰
       </button>
 
       {visible && (
         <div className="menubar">
+          ActionLink()
           <a className="fa fa-instagram" href="https://www.instagram.com/thelillyguildtrust" />
           <a className="fa fa-envelope" href="mailto:terrormarclub@gmail.com" />
           <a className="fa fa-phone" href="tel:+447394074047" />
-          <a href="#" className={isActive(3)} onClick={() => setDemo(3)}>Videos</a>
-          {/* <a
-            key={3}
-            className={isActive(3)}
-            onClick={() => setDemo(3)}>Videos</a> */}
+          <a className="fa fa-camera" onClick={ActionLink}>Videos</a>
         </div>
       )}
     </>
