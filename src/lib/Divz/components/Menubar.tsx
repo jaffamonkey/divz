efe8241dde5c828080ac983e53fba694a30c87f3
jaffamonkey <paullittlebury@gmail.com>
2025-07-15 20:16:45 +0100

    updates

--- src/lib/Divz/components/Menubar.tsx
+++ src/lib/Divz/components/Menubar.tsx
@@ -7,6 +7,11 @@ export const Menubar: React.FC = () => {
     setVisible(prev => !prev);
   };
 
+  const [demo, setDemo] = useState<number>(1);
+
+  const isActive = (link: number) => {
+    return link === demo ? "active" : "";
+  };
   return (
     <>
       <button className="toggle-button" onClick={toggleMenubar}>
@@ -18,6 +23,10 @@ export const Menubar: React.FC = () => {
           <a className="fa fa-instagram" href="https://www.instagram.com/thelillyguildtrust" />
           <a className="fa fa-envelope" href="mailto:terrormarclub@gmail.com" />
           <a className="fa fa-phone" href="tel:+447394074047" />
+          <a
+            key={3}
+            className={isActive(3)}
+            onClick={() => setDemo(3)} />
         </div>
       )}
     </>
