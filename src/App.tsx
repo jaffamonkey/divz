3454663f561cf519d57ab15f23dd3fd1b697131b
Paul Littlebury <paullittlebury@gmail.com>
2025-07-27 14:33:12 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -141,7 +141,7 @@ function Demo3() {
 
       <Divz
         autoPlay={true}
-        autoPlayDuration={10000}
+        autoPlayDuration={5000}
         className="demo3"
         isDarkMode={true}
         isExpanded={true}
@@ -161,14 +161,14 @@ function Demo3() {
         ))}
 
         {/* Example of just using a standard video element that loads immediately */}
-        <div>
+        {/* <div>
           <video autoPlay playsInline loop muted>
             <source
               src="./demo3/1.mp4"
               type="video/mp4"
             />
           </video>
-        </div>
+        </div> */}
       </Divz>
     </>
   );
