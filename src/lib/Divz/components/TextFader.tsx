eb42fa66ccefa6b0a78b671c2bdaceafd5de9c92
Paul Littlebury <paullittlebury@gmail.com>
2025-12-06 10:46:36 +0000

    Update text content in TextFader component

--- src/lib/Divz/components/TextFader.tsx
+++ src/lib/Divz/components/TextFader.tsx
@@ -12,9 +12,9 @@ import { motion, AnimatePresence } from "framer-motion";
 
 const lines = [
     "Welcome to yet another never-ending useless website for a movie that will never get made.",
-    "Scroll through our photos and videos of Spanish stuff. Every few weeks we add a few more we found on Google.",
-    "Web galleries may be dull, but what the hell - we can't be bothered to do too much thinking.",
-    "A magical movie that will never get made because we both already shitting money out of our arses.",
+    "Scroll through our photos and videos of vaguely Spanish stuff. Every few weeks we add a few more we found on Google.",
+    "Web galleries may be dull, but what the hell - we simply have no urgency at all.",
+    "A magical movie that will never get made because we are both already shitting money out of our arses.",
     "Dine on the galleries, or allow the juice Instagram to run down your contented chin"
 ];
 const TextFader: React.FC = () => {
