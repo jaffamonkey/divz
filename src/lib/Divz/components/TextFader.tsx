426a3885023425b5e8137abd621a87de034cdd7b
Paul Littlebury <paullittlebury@gmail.com>
2025-12-06 10:51:57 +0000

    Update text in TextFader component

--- src/lib/Divz/components/TextFader.tsx
+++ src/lib/Divz/components/TextFader.tsx
@@ -13,7 +13,7 @@ import { motion, AnimatePresence } from "framer-motion";
 const lines = [
     "Welcome to yet another never-ending useless website for a movie that will never get made.",
     "Scroll through our photos and videos of vaguely Spanish stuff. Every few weeks we add a few more we found on Google.",
-    "Web galleries may be dull, but what the hell - we simply have no urgency at all.",
+    "Web galleries may be dull, but what the hell - we can't be arsed to try.",
     "A magical movie that will never get made because we are both already shitting money out of our arses.",
     "Dine on the galleries, or allow the juice Instagram to run down your contented chin"
 ];
